805ccdd238dc8e52b59f608b065da701c2214a3e
Arjen de Aldrey <adealdrey1@gmail.com>
2020-05-31 16:34:41 +0200

    [INIT] initial Unity files and a few assets

---
+++ Project CraftMine/Assets/Scripts/FollowBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBlock : MonoBehaviour
+{
+    [SerializeField] private Vector3 Offset;
+    private GameObject block;
+    private Vector3 DesiredLocation;
+    void Start()
+    {
+        block = FindObjectOfType<MovePlayer>().gameObject.GetComponentInChildren<Rigidbody>().gameObject;
+    }
+
+    void Update()
+    {
+        DesiredLocation = block.transform.position;
+        DesiredLocation.y = 0;
+        DesiredLocation += Offset;
+        transform.position = DesiredLocation;
+    }
+}
---
+++ Project CraftMine/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private Rigidbody BlockRig;
+    private Vector3 _movementVector;
+    [SerializeField] private float _speed;
+    void Start()
+    {
+        BlockRig = GetComponentInChildren<Rigidbody>();
+    }
+
+    void Update()
+    {
+        _movementVector = new Vector3(Input.GetAxisRaw("Vertical"), 0, -Input.GetAxisRaw("Horizontal"));
+        BlockRig.AddTorque(_movementVector * _speed * Time.deltaTime);
+    }
+}
